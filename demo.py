b44e7524ee831e3d754b9494627d2d176695c104
DBDBDDB <luodianhit@hotmail.com>
2017-09-21 17:36:20 +0800

    get detailed function

--- demo.py
+++ demo.py
@@ -1,9 +1,29 @@
 import requests
 from bs4 import BeautifulSoup
 
-r = requests.get('http://www.cnydsimtek.com/company')
-soup = BeautifulSoup(r.text, 'html.parser')
-imageURL = soup.find('img', attrs={"data-src": True})
-image = imageURL['data-src']
 
-print(image)
+def getHTML(url):
+    r = requests.get(url)
+    return r
+
+
+def parserHTML(data):
+    soup = BeautifulSoup(data.text, 'html.parser')
+    imageURL = soup.find('img', attrs={"data-src": True})
+    return imageURL['data-src']
+
+
+def saveImage(imageURL):
+    imageData = requests.get('http:'+imageURL)
+    with open('Dr-Ji.jpg', 'wb') as f:
+        f.write(imageData.content)
+
+
+def main():
+    url = "http://www.cnydsimtek.com/company"
+    data = getHTML(url)
+    imageURL = parserHTML(data)
+    saveImage(imageURL)
+
+
+main()
