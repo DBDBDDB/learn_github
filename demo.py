72ad4c633bdf1b7c1d4b157d7a6276bd0d001044
DBDBDDB <luodianhit@hotmail.com>
2017-09-21 17:19:56 +0800

    get image URL

--- demo.py
+++ demo.py
@@ -1,5 +1,9 @@
 import requests
+from bs4 import BeautifulSoup
 
 r = requests.get('http://www.cnydsimtek.com/company')
+soup = BeautifulSoup(r.text, 'html.parser')
+imageURL = soup.find('img', attrs={"data-src": True})
+image = imageURL['data-src']
 
-print(r.text)
+print(image)
